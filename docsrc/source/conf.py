d7b7aed7f38e3d7531410c8f0d21e90f1f4ca6e6
ryans <ryans@jlab.org>
2021-05-03 13:43:47 -0400

    v0.0.6

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Jefferson Lab'
 author = 'Ryan Slominski'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.6'
 
 
 # -- General configuration ---------------------------------------------------
