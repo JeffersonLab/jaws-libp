c2915e5c634ffc6b7c763dd217c6d9bad6aba54b
slominskir <Ryan.Slominski@gmail.com>
2021-10-19 09:48:46 -0500

    EffectiveAlarmSerde

--- src/jlab_jaws/avro/serde.py
+++ src/jlab_jaws/avro/serde.py
@@ -1071,13 +1071,8 @@ class EffectiveAlarmSerde:
         :return: A dict
         """
         return {
-            "class": AlarmClassSerde.to_dict(obj.alarm_class),
-            "registration": AlarmRegistrationSerde.to_dict(obj.registration),
-            "effectiveRegistration": AlarmRegistrationSerde.to_dict(obj.effective_registration),
-            "activation": AlarmActivationUnionSerde.to_dict(obj.activation),
-            "overrides": AlarmOverrideSetSerde.to_dict(obj.overrides),
-            "transitions": ProcessorTransitionsSerde.to_dict(obj.transitions),
-            "state": obj.state.name
+            "registration": EffectiveRegistrationSerde.to_dict(obj.registration),
+            "activation": EffectiveActivationSerde.to_dict(obj.activation),
         }
 
     @staticmethod
@@ -1092,16 +1087,8 @@ class EffectiveAlarmSerde:
         :param the_dict: The dict
         :return: The EffectiveAlarm
         """
-        return EffectiveAlarm(AlarmClassSerde.from_dict(the_dict['class'][1]) if the_dict.get('class') is not None else None,
-                              AlarmRegistrationSerde.from_dict(the_dict['registration'][1])
-                              if the_dict.get('registration') is not None else None,
-                              AlarmRegistrationSerde.from_dict(the_dict['effectiveRegistration'][1])
-                              if the_dict.get('effectiveRegistration') is not None else None,
-                              AlarmActivationUnionSerde.from_dict(the_dict['activation'][1])
-                              if the_dict.get('activation') is not None else None,
-                              AlarmOverrideSetSerde.from_dict(the_dict['overrides']),
-                              ProcessorTransitionsSerde.from_dict(the_dict['transitions']),
-                              _unwrap_enum(the_dict['state'], AlarmState))
+        return EffectiveAlarm(EffectiveRegistrationSerde.from_dict(the_dict['registration']),
+                              EffectiveActivationSerde.from_dict(the_dict['activation']))
 
     @staticmethod
     def _from_dict_with_ctx(the_dict, ctx):
@@ -1111,61 +1098,31 @@ class EffectiveAlarmSerde:
     def references():
         references = []
 
-        classes_schema_ref = SchemaReference("org.jlab.jaws.entity.AlarmClass", "alarm-classes-value", 1)
-        registration_schema_ref = SchemaReference("org.jlab.jaws.entity.AlarmRegistration",
-                                                  "alarm-registrations-value", 1)
-        activation_schema_ref = SchemaReference("org.jlab.jaws.entity.AlarmActivationUnion",
-                                                "alarm-activations-value", 1)
-        overrides_schema_ref = SchemaReference("org.jlab.jaws.entity.AlarmOverrideSet",
-                                               "alarm-override-set", 1)
-        transitions_schema_ref = SchemaReference("org.jlab.jaws.entity.ProcessorTransitions",
-                                                 "processor-transitions", 1)
-        state_schema_ref = SchemaReference("org.jlab.jaws.entity.AlarmState", "alarm-state", 1)
+        registration_schema_ref = SchemaReference("org.jlab.jaws.entity.EffectiveRegistration",
+                                                  "effective-registrations-value", 1)
+        activation_schema_ref = SchemaReference("org.jlab.jaws.entity.EffectiveActivation",
+                                                "effective-activations-value", 1)
 
-        references.append(classes_schema_ref)
         references.append(registration_schema_ref)
         references.append(activation_schema_ref)
-        references.append(overrides_schema_ref)
-        references.append(transitions_schema_ref)
-        references.append(state_schema_ref)
 
         return references
 
     @staticmethod
     def named_schemas():
-        classes_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/AlarmClass.avsc")
-        classes_schema_str = classes_bytes.decode('utf-8')
-
-        registrations_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/AlarmRegistration.avsc")
+        registrations_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/EffectiveRegistration.avsc")
         registrations_schema_str = registrations_bytes.decode('utf-8')
 
-        activation_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/AlarmActivationUnion.avsc")
+        activation_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/EffectiveActivation.avsc")
         activation_schema_str = activation_bytes.decode('utf-8')
 
-        overrides_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/AlarmOverrideSet.avsc")
-        overrides_schema_str = overrides_bytes.decode('utf-8')
+        named_schemas = {}
+        # named_schemas = EffectiveRegistrationSerde.named_schemas()
+        # named_schemas.update(EffectiveActivationSerde.named_schemas())
 
-        transitions_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/ProcessorTransitions.avsc")
-        transitions_schema_str = transitions_bytes.decode('utf-8')
-
-        state_bytes = pkgutil.get_data("jlab_jaws", "avro/schemas/AlarmState.avsc")
-        state_schema_str = state_bytes.decode('utf-8')
-
-        named_schemas = AlarmClassSerde.named_schemas()
-
-        named_schemas.update(AlarmOverrideUnionSerde.named_schemas())
-
-        ref_dict = loads(classes_schema_str)
-        parse_schema(ref_dict, named_schemas=named_schemas)
         ref_dict = loads(registrations_schema_str)
         parse_schema(ref_dict, named_schemas=named_schemas)
         ref_dict = loads(activation_schema_str)
-        parse_schema(ref_dict, named_schemas=named_schemas)
-        ref_dict = loads(overrides_schema_str)
-        parse_schema(ref_dict, named_schemas=named_schemas)
-        ref_dict = loads(transitions_schema_str)
-        parse_schema(ref_dict, named_schemas=named_schemas)
-        ref_dict = loads(state_schema_str)
         parse_schema(ref_dict, named_schemas=named_schemas)
 
         return named_schemas
