f80fdfdd2c007575cb605beda313f09917fbc5e0
slominskir <Ryan.Slominski@gmail.com>
2021-11-10 13:58:38 -0500

    2.4.0

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Jefferson Lab'
 author = 'Ryan Slominski'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.0'
+release = '2.4.0'
 
 
 # -- General configuration ---------------------------------------------------
