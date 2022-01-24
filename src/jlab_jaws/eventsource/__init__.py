13296eb3b37e727748762977872684e2355d712c
ryans <ryans@jlab.org>
2022-01-24 09:10:54 -0500

    Re-organized package

--- src/jlab_jaws/eventsource/__init__.py
+++ src/jlab_jaws/eventsource/__init__.py
@@ -5,3 +5,18 @@
        - `Storing Data in Kafka <https://www.confluent.io/blog/okay-store-data-apache-kafka/>`_
        - `Fowler on Event Sourcing <ttps://martinfowler.com/eaaDev/EventSourcing.html>`_
 """
+import logging
+
+from .listener import EventSourceListener
+from .table import EventSourceTable
+from .cached_table import CachedTable
+
+logger = logging.getLogger(__name__)
+
+
+class TimeoutException(Exception):
+    pass
+
+
+def log_exception(e):
+    logger.exception(e)
