0526fec4cb402bc0b0ac866782b492c46e4c7278
ryans <ryans@jlab.org>
2022-01-24 16:31:04 -0500

    docs

--- src/jlab_jaws/eventsource/__init__.py
+++ src/jlab_jaws/eventsource/__init__.py
@@ -3,5 +3,5 @@
 
    See Also:
        - `Storing Data in Kafka <https://www.confluent.io/blog/okay-store-data-apache-kafka/>`_
-       - `Fowler on Event Sourcing <ttps://martinfowler.com/eaaDev/EventSourcing.html>`_
+       - `Fowler on Event Sourcing <https://martinfowler.com/eaaDev/EventSourcing.html>`_
 """
