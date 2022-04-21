6529f2b13040b7ceb4777fe4adcae576922f4879
slominskir <ryan.slominski@gmail.com>
2022-04-21 09:04:00 -0400

    v4.2.0

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Jefferson Lab'
 author = 'Ryan Slominski'
 
 # The full version, including alpha/beta/rc tags
-release = '4.1.0'
+release = '4.2.0'
 
 
 # -- General configuration ---------------------------------------------------
