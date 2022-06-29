e5d965e7411701ca6ae831e040e09fc7f6a5c934
slominskir <ryan.slominski@gmail.com>
2022-06-29 10:46:58 -0400

    Updated test

--- tests/test_serde.py
+++ tests/test_serde.py
@@ -6,8 +6,8 @@ def test_instance_serde():
 
     serde = InstanceSerde(None, union_encoding=UnionEncoding.DICT_WITH_TYPE)
 
-    expected_json = '{"class": "base", "location": ["INJ"], "maskedby": null, "producer": {' \
-                    '"org.jlab.jaws.entity.EPICSProducer": {"pv": "channel1"}}, "screencommand": "/"}'
+    expected_json = '{"alarmclass": "base", "location": ["INJ"], "maskedby": null, "screencommand": "/", "source": {' \
+                    '"org.jlab.jaws.entity.EPICSSource": {"pv": "channel1"}}}'
 
     entity = serde.from_json(expected_json)
 
