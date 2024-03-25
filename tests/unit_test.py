79d9e2cc01a5f28363ed50ab8da4beae92b1fb0b
ryans <ryans@jlab.org>
2024-03-25 15:40:23 -0400

    Avoid exit code 5

---
+++ tests/unit_test.py
@@ -0,0 +1,8 @@
+import pytest
+
+# You MUST have at least 1 unit test else pytest fails with exit code 5
+# https://github.com/pytest-dev/pytest/issues/2393
+
+@pytest.mark.unit
+def test_one():
+    pass
