4258500a99b20b8836591a1f128aebb155955f23
Ryan Slominski <Ryan.Slominski@gmail.com>
2024-08-27 14:09:12 -0400

    AlarmCategory -> AlarmSystem

--- tests/integration/test_action.py
+++ tests/integration/test_action.py
@@ -7,19 +7,19 @@ from jaws_libp.scripts.client.set_action import set_action
 
 
 def test_action():
-    category = 'EXAMPLE_CATEGORY'
+    system = 'EXAMPLE_SYSTEM'
     action_name = "TESTING_ACTION"
-    action = AlarmAction(category, AlarmPriority.P3_MINOR, 'TESTING_RATIONALE',
+    action = AlarmAction(system, AlarmPriority.P3_MINOR, 'TESTING_RATIONALE',
                              'TESTING_CORRECTIVE_ACTION', True, True, None, None)
 
     runner = CliRunner()
 
-    set_action.params[2].type = Choice([category])
+    set_action.params[2].type = Choice([system])
 
     try:
         # Set
         result = runner.invoke(set_action, [action_name,
-                                           '--category', action.category,
+                                           '--system', action.system,
                                            '--priority', action.priority.name,
                                            '--rationale', action.rationale,
                                            '--correctiveaction', action.corrective_action])
--- tests/integration/test_system_async.py
+++ tests/integration/test_system_async.py
@@ -2,34 +2,34 @@ from typing import Dict, Any, List
 
 from confluent_kafka import Message
 
-from jaws_libp.clients import CategoryProducer, CategoryConsumer
+from jaws_libp.clients import SystemProducer, SystemConsumer
 from jaws_libp.eventsource import EventSourceListener
-from jaws_libp.entities import AlarmCategory
+from jaws_libp.entities import AlarmSystem
 
-class CategoryListener(EventSourceListener):
-    _categories: Dict[Any, Message]
+class SystemListener(EventSourceListener):
+    _systems: Dict[Any, Message]
 
     def on_highwater(self, cache: Dict[Any, Message]) -> None:
-        self._categories = cache
+        self._systems = cache
 
     def on_batch(self, msgs: List[Message], highwater_reached: bool) -> None:
         pass
 
     def get_categories(self) -> Dict[Any, Message]:
-        return self._categories
+        return self._systems
 
 
 def test_category_client():
-    producer = CategoryProducer('category-test')
-    consumer = CategoryConsumer('category-test')
+    producer = SystemProducer('category-test')
+    consumer = SystemConsumer('category-test')
 
     expected_key = "TESTING"
-    expected_value = AlarmCategory("Tester")
+    expected_value = AlarmSystem("Tester")
 
     try:
         producer.send(expected_key, expected_value)
 
-        listener = CategoryListener()
+        listener = SystemListener()
 
         consumer.add_listener(listener)
 
